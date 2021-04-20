8ea6985c58ac12eb0921585caba555e6c1dec317
GhitaB <ghitabizau27@gmail.com>
2021-04-20 09:34:35 +0300

    WIP board.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lines: [
+        [0],
+        [0, 0],
+        [0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="game-board-container">
-        <div id="game-board">Game board</div>
+        <div id="game-board">
+          <p>Game board</p>
+          <div>{this.state.lines}</div>
+        </div>
       </div>
     );
   }
