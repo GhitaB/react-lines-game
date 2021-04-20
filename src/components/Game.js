f8950666458bc32a01a3764e9d1ef77078e315ee
GhitaB <ghitabizau27@gmail.com>
2021-04-20 15:17:05 +0300

    Players.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 
 function LineItem({value}) {
@@ -19,38 +19,54 @@ function LinesRow({items}) {
   ));
 }
 
+function Game() {
+  const [lines, setLines] = React.useState([
+    [0],
+    [0, 0],
+    [0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]);
 
-class Game extends Component {
-  constructor() {
-    super();
-    this.state = {
-      lines: [
-        [0],
-        [0, 0],
-        [0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-      ]
-    }
-  }
+  const [players, setPlayers] = React.useState({
+    player1: {name: 'Player 1', isPlaying: true},
+    player2: {name: 'Player 2', isPlaying: false}
+  });
 
-  render() {
-    var lines = this.state.lines;
-    var linesListing = lines.map(function(line, index) {
-      return (<li key={index}><LinesRow items={line} /></li>);
+  const changePlayer = () => {
+    var player1Play = !players.player1.isPlaying;
+    var player2Play = !players.player2.isPlaying;
+
+    setPlayers({
+      player1: {name: 'Player 1', isPlaying: player1Play},
+      player2: {name: 'Player 2', isPlaying: player2Play}
     });
-
-    return (
-      <div className="game-board-container">
-        <div id="game-board">
-          <p>Game board</p>
-
-          <ul>{linesListing}</ul>
-        </div>
-      </div>
-    );
   }
+
+  var linesListing = lines.map(function(line, index) {
+    return (<li key={index}><LinesRow items={line} /></li>);
+  });
+
+  return (
+    <div className="game-board-container">
+      <div id="game-board">
+        <p>Game board</p>
+
+        <ul>{linesListing}</ul>
+
+        <button onClick={changePlayer} className="btn btn-primary">Play</button>
+        <h4>
+          <span>{players.player1.name}</span>:
+          <span> {players.player1.isPlaying ? "is playing" : ""}</span>
+        </h4>
+        <h4>
+          <span>{players.player2.name}</span>:
+          <span> {players.player2.isPlaying ? "is playing" : ""}</span>
+        </h4>
+      </div>
+    </div>
+  );
 }
 
 export default Game;
